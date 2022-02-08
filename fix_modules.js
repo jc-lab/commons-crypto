eb94f15df91d7adf880753be2549659a3729fe26
Joseph Lee <joseph@jc-lab.net>
2022-02-08 16:10:56 +0900

    fix: fix_modules

--- fix_modules.js
+++ fix_modules.js
@@ -1,6 +1,14 @@
 const path = require('path');
 const fs = require('fs');
 
-fs.rmdirSync(path.resolve('./node_modules/pkijs/node_modules') , {
-    recursive: true
-});
+try {
+  // hoisting
+  const pkijsNodeModulesDir = path.resolve('./node_modules/pkijs/node_modules');
+  if (fs.existsSync(pkijsNodeModulesDir)) {
+    fs.rmdirSync(pkijsNodeModulesDir , {
+        recursive: true
+    });
+  }
+} catch (e) {
+  console.warn(e);
+}
